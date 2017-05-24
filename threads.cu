c9f3950cf0ada12a2ce1d7fb026d69f48c7c9fe6
Eric Caleb <calebme@mail.com>
2017-05-24 12:23:36 +0000

    handle threads

---
+++ threads.cu
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "./random.h"
+
+#define N 512
+
+__global__ void add(int *a, int *b, int *c) {
+  c[threadIdx.x] = a[threadIdx.x] + b[threadIdx.x];
+}
+
+int main(void) {
+    int *a, *b ,*c;
+    int *d_a, *d_b, *d_c ;
+    int size = N * sizeof(int);
+
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_b, size);
+    cudaMalloc((void **)&d_c, size);
+
+    // Alloc space for host copies of a, b, c and setup input values
+    a = (int *)malloc(size); random_ints(a, N);
+    b = (int *)malloc(size); random_ints(b, N);
+    c = (int *)malloc(size);
+
+    // Copy inputs to device
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+
+    // Launch add() kernel on GPU with multiple threads
+    add<<<1,N>>>(d_a, d_b, d_c);
+
+    // Copy result back to host
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+
+    // Cleanup
+    free(a); free(b); free(c);
+    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+    return 0;
+}
